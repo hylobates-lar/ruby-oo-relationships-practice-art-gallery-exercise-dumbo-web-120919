e1bc20f7aea6fb72d88271f7c8a1dec0e1b7c244
hylobates-lar <alison.v.quaglia@gmail.com>
2019-12-17 14:18:46 -0500

    all tests passing

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,54 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+
+    @@all << self
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def all_paintings
+    Painting.all.select do |painting_instance|
+      painting_instance.artist == self 
+    end
+  end
+
+  def all_galleries
+    self.all_paintings.map do |painting_instance|
+      painting_instance.gallery
+    end
+  end
+
+  def all_cities
+    self.all_galleries.map do |gallery|
+      gallery.city
+    end
+  end
+
+  def self.total_experience
+    total = 0
+
+    self.all.map do |artist|
+      total += artist.years_experience
+    end
+    total 
+  end
+
+  def self.most_prolific
+    self.all.max_by do |artist|
+      artist.paintings.length / artist.years_experience
+    end 
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,17 @@
 require_relative '../config/environment.rb'
 
+a1 = Artist.new("Susan", 5)
+a2 = Artist.new("Lily", 10)
+a3 = Artist.new("Mark", 20)
 
+g1 = Gallery.new("Gallery A", "Boston")
+g2 = Gallery.new("Gallery B", "Paris")
+g3 = Gallery.new("Gallery C", "New York")
+
+p1 = Painting.new("Fawn", 1000, a1, g3)
+p2 = Painting.new("Lamb", 1500, a2, g1)
+p3 = Painting.new("Boar", 3000, a3, g2)
+p4 = Painting.new("Goat", 400, a1, g2)
 
 
 binding.pry
